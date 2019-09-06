a5101a78b938aa4090a565073d1f546463df8c3b
Harry Liu <byliuyang11@gmail.com>
2019-09-06 00:37:12 -0700

    Add assertion (#13)

---
+++ mdtest/assertion.go
@@ -0,0 +1,15 @@
+package mdtest
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Equal(t *testing.T, expected, actual interface{}, msgAndArgs ...interface{}) bool {
+	return assert.Equal(t, expected, actual, msgAndArgs)
+}
+
+func NotEqual(t *testing.T, expected, actual interface{}, msgAndArgs ...interface{}) bool {
+	return assert.NotEqual(t, expected, actual, msgAndArgs)
+}
