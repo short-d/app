9d74b281c0c7e1f7b6d97c55308d6659d6589a43
Harry Liu <byliuyang11@gmail.com>
2019-10-22 00:09:12 -0700

    Add Environment (#32)

---
+++ modern/mdenv/env.go
@@ -0,0 +1,38 @@
+package mdenv
+
+import (
+	"log"
+	"os"
+
+	"github.com/byliuyang/app/fw"
+	"github.com/joho/godotenv"
+)
+
+var _ fw.Environment = (*GoDotEnv)(nil)
+
+type GoDotEnv struct {
+}
+
+func (g GoDotEnv) GetEnv(key string, defaultValue string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		return defaultValue
+	}
+	return val
+}
+
+func (g GoDotEnv) AutoLoadDotEnvFile() {
+	_, err := os.Stat(".env")
+	if os.IsNotExist(err) {
+		return
+	}
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func NewGoDotEnv() GoDotEnv {
+	return GoDotEnv{}
+}
