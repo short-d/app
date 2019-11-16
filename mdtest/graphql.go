2469f28756353dc2f1a1e9c7193b7eb7d6abc011
Harry Liu <byliuyang11@gmail.com>
2019-11-15 22:33:37 -0800

    Add GraphQLServerFake

--- mdtest/graphql.go
+++ mdtest/graphql.go
@@ -1,12 +1,34 @@
 package mdtest
 
 import (
-	"github.com/byliuyang/app/fw"
+	"net/http/httptest"
 
+	"github.com/byliuyang/app/fw"
 	"github.com/graph-gophers/graphql-go"
+	"github.com/graph-gophers/graphql-go/relay"
 )
 
 func IsGraphQlAPIValid(api fw.GraphQlAPI) bool {
 	_, err := graphql.ParseSchema(api.GetSchema(), api.GetResolver())
 	return err == nil
+}
+
+type GraphQLServerFake struct {
+	server *httptest.Server
+}
+
+func (g GraphQLServerFake) URL() string {
+	return g.server.URL
+}
+
+func NewGraphQLServerFake(api fw.GraphQlAPI) GraphQLServerFake {
+	schema := graphql.MustParseSchema(api.GetSchema(), api.GetResolver())
+	relayHandler := relay.Handler{
+		Schema: schema,
+	}
+
+	server := httptest.NewServer(&relayHandler)
+	return GraphQLServerFake{
+		server: server,
+	}
 }
