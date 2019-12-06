5b7505ba85cd67d3f0f56f09bd17a0d67509c10a
Harry Liu <byliuyang11@gmail.com>
2019-12-05 22:14:39 -0800

    Add AccessTestDB test helper (#43)
    * Add AccessTestDB test helper
    
    * Format code
    
    * Address code review comments

---
+++ mdtest/db.go
@@ -0,0 +1,59 @@
+package mdtest
+
+import (
+	"database/sql"
+	"os"
+	"path/filepath"
+
+	"github.com/byliuyang/app/fw"
+)
+
+type dbConsumer func(sqlDB *sql.DB)
+
+func AccessTestDB(
+	dbConnector fw.DBConnector,
+	dbMigrationTool fw.DBMigrationTool,
+	dbMigrationRoot string,
+	dbConfig fw.DBConfig,
+	consumer dbConsumer) {
+
+	workDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	dbMigrationRoot = filepath.Join(workDir, dbMigrationRoot)
+
+	db, err := dbConnector.Connect(dbConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	err = resetDatabase(db, dbMigrationRoot, dbMigrationTool)
+	if err != nil {
+		panic(err)
+	}
+
+	err = dbMigrationTool.MigrateUp(db, dbMigrationRoot)
+	if err != nil {
+		panic(err)
+	}
+
+	consumer(db)
+
+	err = dbMigrationTool.MigrateDown(db, dbMigrationRoot)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func resetDatabase(db *sql.DB, dbMigrationRoot string, dbMigrationTool fw.DBMigrationTool) error {
+	err := dbMigrationTool.MigrateUp(db, dbMigrationRoot)
+	if err != nil {
+		return err
+	}
+
+	return dbMigrationTool.MigrateDown(db, dbMigrationRoot)
+}
