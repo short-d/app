3740d89f75c5144ca74fc9cd64dec568c4f15db0
Harry Liu <byliuyang11@gmail.com>
2020-01-05 02:07:27 -0800

    Add build in stdin and stdout (#54)
    * Add build in stdin and stdout
    
    * Clean up

--- modern/mdio/io.go
+++ modern/mdio/io.go
@@ -4,7 +4,36 @@ import (
 	"bytes"
 	"io"
 	"io/ioutil"
+	"os"
+
+	"github.com/short-d/app/fw"
 )
+
+var _ fw.StdIn = (*StdIn)(nil)
+
+type StdIn struct {
+}
+
+func (s StdIn) Read(p []byte) (n int, err error) {
+	return os.Stdin.Read(p)
+}
+
+func NewBuildInStdIn() StdIn {
+	return StdIn{}
+}
+
+var _ fw.StdOut = (*StdOut)(nil)
+
+type StdOut struct {
+}
+
+func (s StdOut) Write(p []byte) (n int, err error) {
+	return os.Stdout.Write(p)
+}
+
+func NewBuildInStdOut() StdOut {
+	return StdOut{}
+}
 
 func Tap(r io.ReadCloser, fn func(text string)) io.ReadCloser {
 	buf, _ := ioutil.ReadAll(r)
