dbfc79d26f946f2af2e1249bd3633b84ecb5d346
Harry Liu <byliuyang11@gmail.com>
2020-01-05 00:54:25 -0800

    Support multiple log levels (#49)
    * Support multiple log levels
    
    * Format code
    
    * Extra external dependencies out of logger
    
    * Add unit tests

--- modern/mdlogger/local.go
+++ modern/mdlogger/local.go
@@ -1,26 +1,146 @@
 package mdlogger
 
 import (
-	"log"
+	"fmt"
 
 	"github.com/byliuyang/app/fw"
 )
 
+var _ fw.Logger = (*Local)(nil)
+
+const datetimeFormat = "2006-01-02 15:04:05"
+
 type Local struct {
+	prefix         string
+	level          fw.LogLevel
+	stdout         fw.StdOut
+	timer          fw.Timer
+	programRuntime fw.ProgramRuntime
 }
 
-func (Local) Error(err error) {
-	log.Println(err.Error())
+func (local Local) Fatal(message string) {
+	if local.levelAboveFatal() {
+		return
+	}
+	local.log(fw.LogFatalName, message)
 }
 
-func (Local) Info(info string) {
-	log.Println(info)
+func (local Local) Error(err error) {
+	if local.levelAboveError() {
+		return
+	}
+	local.log(fw.LogErrorName, fmt.Sprintf("%v", err))
 }
 
-func (Local) Crash(err error) {
-	log.Fatal(err)
+func (local Local) Warn(message string) {
+	if local.levelAboveWarn() {
+		return
+	}
+	local.log(fw.LogWarnName, message)
 }
 
-func NewLocal() fw.Logger {
-	return Local{}
+func (local Local) Info(message string) {
+	if local.levelAboveInfo() {
+		return
+	}
+	local.log(fw.LogInfoName, message)
+}
+
+func (local Local) Debug(message string) {
+	if local.levelAboveDebug() {
+		return
+	}
+	local.log(fw.LogDebugName, message)
+}
+
+func (local Local) Trace(message string) {
+	if local.levelAboveTrace() {
+		return
+	}
+	local.log(fw.LogTraceName, message)
+}
+
+func (local Local) log(level fw.LogLevelName, message string) {
+	now := local.now()
+	caller, err := local.programRuntime.Caller(2)
+	if err != nil {
+		_, _ = fmt.Fprintf(
+			local.stdout,
+			"[%s] [%s] %s %s\n",
+			local.prefix,
+			level,
+			now,
+			message,
+		)
+		return
+	}
+	_, _ = fmt.Fprintf(
+		local.stdout,
+		"[%s] [%s] %s line %d at %s %s\n",
+		local.prefix,
+		level,
+		now,
+		caller.LineNumber,
+		caller.FullFilename,
+		message,
+	)
+}
+
+func (local Local) now() string {
+	return local.timer.Now().UTC().Format(datetimeFormat)
+}
+
+func (local Local) levelAboveFatal() bool {
+	return local.level == fw.LogOff
+}
+
+func (local Local) levelAboveError() bool {
+	if local.levelAboveFatal() {
+		return true
+	}
+	return local.level == fw.LogFatal
+}
+
+func (local Local) levelAboveWarn() bool {
+	if local.levelAboveError() {
+		return true
+	}
+	return local.level == fw.LogError
+}
+
+func (local Local) levelAboveInfo() bool {
+	if local.levelAboveWarn() {
+		return true
+	}
+	return local.level == fw.LogWarn
+}
+
+func (local Local) levelAboveDebug() bool {
+	if local.levelAboveInfo() {
+		return true
+	}
+	return local.level == fw.LogInfo
+}
+
+func (local Local) levelAboveTrace() bool {
+	if local.levelAboveDebug() {
+		return true
+	}
+	return local.level == fw.LogDebug
+}
+
+func NewLocal(
+	prefix string,
+	level fw.LogLevel,
+	stdout fw.StdOut,
+	timer fw.Timer,
+	programRuntime fw.ProgramRuntime,
+) Local {
+	return Local{
+		prefix:         prefix,
+		level:          level,
+		stdout:         stdout,
+		timer:          timer,
+		programRuntime: programRuntime,
+	}
 }
---
+++ modern/mdlogger/local_test.go
@@ -0,0 +1,653 @@
+package mdlogger
+
+import (
+	"bytes"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/byliuyang/app/fw"
+	"github.com/byliuyang/app/mdtest"
+)
+
+func TestLocal_Fatal(t *testing.T) {
+	testCases := []struct {
+		name           string
+		logLevel       fw.LogLevel
+		now            string
+		prefix         string
+		messages       []string
+		callers        []fw.Caller
+		expectedOutput string
+	}{
+		{
+			name:           "logging disabled",
+			logLevel:       fw.LogOff,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{"message"},
+			callers:        []fw.Caller{},
+			expectedOutput: "",
+		},
+		{
+			name:     "log fatal message",
+			logLevel: fw.LogFatal,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message 1", "message 2"},
+			callers: []fw.Caller{{}, {}, {
+				FullFilename: "app/test.go",
+				LineNumber:   10,
+			}},
+			expectedOutput: `[Prefix] [Fatal] 2020-01-04 10:20:04 line 10 at app/test.go message 1
+[Prefix] [Fatal] 2020-01-04 10:20:04 line 10 at app/test.go message 2
+`,
+		},
+		{
+			name:     "log error message",
+			logLevel: fw.LogError,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Fatal] 2020-01-04 10:20:04 line 10 at app/test.go message
+`,
+		},
+		{
+			name:     "log warn message",
+			logLevel: fw.LogWarn,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Fatal] 2020-01-04 10:20:04 line 10 at app/test.go message
+`,
+		},
+		{
+			name:     "log info message",
+			logLevel: fw.LogInfo,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Fatal] 2020-01-04 10:20:04 line 10 at app/test.go message
+`,
+		},
+		{
+			name:     "log debug message",
+			logLevel: fw.LogDebug,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Fatal] 2020-01-04 10:20:04 line 10 at app/test.go message
+`,
+		},
+		{
+			name:     "log trace message",
+			logLevel: fw.LogTrace,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Fatal] 2020-01-04 10:20:04 line 10 at app/test.go message
+`,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			now, err := time.Parse(time.RFC3339, testCase.now)
+			mdtest.Equal(t, nil, err)
+			timer := mdtest.NewTimerFake(now)
+
+			programRuntime, err := mdtest.NewProgramRuntimeFake(testCase.callers)
+			mdtest.Equal(t, nil, err)
+
+			var buf bytes.Buffer
+			logger := NewLocal(
+				testCase.prefix,
+				testCase.logLevel,
+				&buf,
+				timer,
+				programRuntime,
+			)
+
+			for _, message := range testCase.messages {
+				logger.Fatal(message)
+			}
+			gotOutput := buf.String()
+			mdtest.Equal(t, testCase.expectedOutput, gotOutput)
+		})
+	}
+}
+
+func TestLocal_Error(t *testing.T) {
+	testCases := []struct {
+		name           string
+		logLevel       fw.LogLevel
+		now            string
+		prefix         string
+		messages       []string
+		callers        []fw.Caller
+		expectedOutput string
+	}{
+		{
+			name:           "logging disabled",
+			logLevel:       fw.LogOff,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{"message"},
+			callers:        []fw.Caller{},
+			expectedOutput: "",
+		},
+		{
+			name:           "log fatal message",
+			logLevel:       fw.LogFatal,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{},
+			callers:        []fw.Caller{},
+			expectedOutput: "",
+		},
+		{
+			name:     "log error message",
+			logLevel: fw.LogError,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message 1", "message 2"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Error] 2020-01-04 10:20:04 line 10 at app/test.go message 1
+[Prefix] [Error] 2020-01-04 10:20:04 line 10 at app/test.go message 2
+`,
+		},
+		{
+			name:     "log warn message",
+			logLevel: fw.LogWarn,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Error] 2020-01-04 10:20:04 line 10 at app/test.go message
+`,
+		},
+		{
+			name:     "log info message",
+			logLevel: fw.LogInfo,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Error] 2020-01-04 10:20:04 line 10 at app/test.go message
+`,
+		},
+		{
+			name:     "log debug message",
+			logLevel: fw.LogDebug,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Error] 2020-01-04 10:20:04 line 10 at app/test.go message
+`,
+		},
+		{
+			name:     "log trace message",
+			logLevel: fw.LogTrace,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Error] 2020-01-04 10:20:04 line 10 at app/test.go message
+`,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			now, err := time.Parse(time.RFC3339, testCase.now)
+			mdtest.Equal(t, nil, err)
+			timer := mdtest.NewTimerFake(now)
+
+			programRuntime, err := mdtest.NewProgramRuntimeFake(testCase.callers)
+			mdtest.Equal(t, nil, err)
+
+			var buf bytes.Buffer
+			logger := NewLocal(
+				testCase.prefix,
+				testCase.logLevel,
+				&buf,
+				timer,
+				programRuntime,
+			)
+
+			for _, message := range testCase.messages {
+				logger.Error(errors.New(message))
+			}
+			gotOutput := buf.String()
+			mdtest.Equal(t, testCase.expectedOutput, gotOutput)
+		})
+	}
+}
+
+func TestLocal_Warn(t *testing.T) {
+	testCases := []struct {
+		name           string
+		logLevel       fw.LogLevel
+		now            string
+		prefix         string
+		messages       []string
+		callers        []fw.Caller
+		expectedOutput string
+	}{
+		{
+			name:           "logging disabled",
+			logLevel:       fw.LogOff,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{"message"},
+			callers:        []fw.Caller{},
+			expectedOutput: "",
+		},
+		{
+			name:           "log fatal message",
+			logLevel:       fw.LogFatal,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{},
+			callers:        []fw.Caller{},
+			expectedOutput: "",
+		},
+		{
+			name:           "log error message",
+			logLevel:       fw.LogError,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{"message"},
+			callers:        []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: "",
+		},
+		{
+			name:     "log warn message",
+			logLevel: fw.LogWarn,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message 1", "message 2"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Warn] 2020-01-04 10:20:04 line 10 at app/test.go message 1
+[Prefix] [Warn] 2020-01-04 10:20:04 line 10 at app/test.go message 2
+`,
+		},
+		{
+			name:     "log info message",
+			logLevel: fw.LogInfo,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Warn] 2020-01-04 10:20:04 line 10 at app/test.go message
+`,
+		},
+		{
+			name:     "log debug message",
+			logLevel: fw.LogDebug,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Warn] 2020-01-04 10:20:04 line 10 at app/test.go message
+`,
+		},
+		{
+			name:     "log trace message",
+			logLevel: fw.LogTrace,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Warn] 2020-01-04 10:20:04 line 10 at app/test.go message
+`,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			now, err := time.Parse(time.RFC3339, testCase.now)
+			mdtest.Equal(t, nil, err)
+			timer := mdtest.NewTimerFake(now)
+
+			programRuntime, err := mdtest.NewProgramRuntimeFake(testCase.callers)
+			mdtest.Equal(t, nil, err)
+
+			var buf bytes.Buffer
+			logger := NewLocal(
+				testCase.prefix,
+				testCase.logLevel,
+				&buf,
+				timer,
+				programRuntime,
+			)
+
+			for _, message := range testCase.messages {
+				logger.Warn(message)
+			}
+			gotOutput := buf.String()
+			mdtest.Equal(t, testCase.expectedOutput, gotOutput)
+		})
+	}
+}
+
+func TestLocal_Info(t *testing.T) {
+	testCases := []struct {
+		name           string
+		logLevel       fw.LogLevel
+		now            string
+		prefix         string
+		messages       []string
+		callers        []fw.Caller
+		expectedOutput string
+	}{
+		{
+			name:           "logging disabled",
+			logLevel:       fw.LogOff,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{"message"},
+			callers:        []fw.Caller{},
+			expectedOutput: "",
+		},
+		{
+			name:           "log fatal message",
+			logLevel:       fw.LogFatal,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{},
+			callers:        []fw.Caller{},
+			expectedOutput: "",
+		},
+		{
+			name:           "log error message",
+			logLevel:       fw.LogError,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{"message"},
+			callers:        []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: "",
+		},
+		{
+			name:           "log warn message",
+			logLevel:       fw.LogWarn,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{"message"},
+			callers:        []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: "",
+		},
+		{
+			name:     "log info message",
+			logLevel: fw.LogInfo,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message 1", "message 2"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Info] 2020-01-04 10:20:04 line 10 at app/test.go message 1
+[Prefix] [Info] 2020-01-04 10:20:04 line 10 at app/test.go message 2
+`,
+		},
+		{
+			name:     "log debug message",
+			logLevel: fw.LogDebug,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Info] 2020-01-04 10:20:04 line 10 at app/test.go message
+`,
+		},
+		{
+			name:     "log trace message",
+			logLevel: fw.LogTrace,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Info] 2020-01-04 10:20:04 line 10 at app/test.go message
+`,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			now, err := time.Parse(time.RFC3339, testCase.now)
+			mdtest.Equal(t, nil, err)
+			timer := mdtest.NewTimerFake(now)
+
+			programRuntime, err := mdtest.NewProgramRuntimeFake(testCase.callers)
+			mdtest.Equal(t, nil, err)
+
+			var buf bytes.Buffer
+			logger := NewLocal(
+				testCase.prefix,
+				testCase.logLevel,
+				&buf,
+				timer,
+				programRuntime,
+			)
+
+			for _, message := range testCase.messages {
+				logger.Info(message)
+			}
+			gotOutput := buf.String()
+			mdtest.Equal(t, testCase.expectedOutput, gotOutput)
+		})
+	}
+}
+
+func TestLocal_Debug(t *testing.T) {
+	testCases := []struct {
+		name           string
+		logLevel       fw.LogLevel
+		now            string
+		prefix         string
+		messages       []string
+		callers        []fw.Caller
+		expectedOutput string
+	}{
+		{
+			name:           "logging disabled",
+			logLevel:       fw.LogOff,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{"message"},
+			callers:        []fw.Caller{},
+			expectedOutput: "",
+		},
+		{
+			name:           "log fatal message",
+			logLevel:       fw.LogFatal,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{},
+			callers:        []fw.Caller{},
+			expectedOutput: "",
+		},
+		{
+			name:           "log error message",
+			logLevel:       fw.LogError,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{"message"},
+			callers:        []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: "",
+		},
+		{
+			name:           "log warn message",
+			logLevel:       fw.LogWarn,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{"message"},
+			callers:        []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: "",
+		},
+		{
+			name:           "log info message",
+			logLevel:       fw.LogInfo,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{"message"},
+			callers:        []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: "",
+		},
+		{
+			name:     "log debug message",
+			logLevel: fw.LogDebug,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message 1", "message 2"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Debug] 2020-01-04 10:20:04 line 10 at app/test.go message 1
+[Prefix] [Debug] 2020-01-04 10:20:04 line 10 at app/test.go message 2
+`,
+		},
+		{
+			name:     "log trace message",
+			logLevel: fw.LogTrace,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Debug] 2020-01-04 10:20:04 line 10 at app/test.go message
+`,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			now, err := time.Parse(time.RFC3339, testCase.now)
+			mdtest.Equal(t, nil, err)
+			timer := mdtest.NewTimerFake(now)
+
+			programRuntime, err := mdtest.NewProgramRuntimeFake(testCase.callers)
+			mdtest.Equal(t, nil, err)
+
+			var buf bytes.Buffer
+			logger := NewLocal(
+				testCase.prefix,
+				testCase.logLevel,
+				&buf,
+				timer,
+				programRuntime,
+			)
+
+			for _, message := range testCase.messages {
+				logger.Debug(message)
+			}
+			gotOutput := buf.String()
+			mdtest.Equal(t, testCase.expectedOutput, gotOutput)
+		})
+	}
+}
+
+func TestLocal_Trace(t *testing.T) {
+	testCases := []struct {
+		name           string
+		logLevel       fw.LogLevel
+		now            string
+		prefix         string
+		messages       []string
+		callers        []fw.Caller
+		expectedOutput string
+	}{
+		{
+			name:           "logging disabled",
+			logLevel:       fw.LogOff,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{"message"},
+			callers:        []fw.Caller{},
+			expectedOutput: "",
+		},
+		{
+			name:           "log fatal message",
+			logLevel:       fw.LogFatal,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{},
+			callers:        []fw.Caller{},
+			expectedOutput: "",
+		},
+		{
+			name:           "log error message",
+			logLevel:       fw.LogError,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{"message"},
+			callers:        []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: "",
+		},
+		{
+			name:           "log warn message",
+			logLevel:       fw.LogWarn,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{"message"},
+			callers:        []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: "",
+		},
+		{
+			name:           "log info message",
+			logLevel:       fw.LogInfo,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{"message"},
+			callers:        []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: "",
+		},
+		{
+			name:           "log debug message",
+			logLevel:       fw.LogDebug,
+			now:            "2020-01-04T10:20:04-00:00",
+			prefix:         "Prefix",
+			messages:       []string{"message"},
+			callers:        []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: "",
+		},
+		{
+			name:     "log trace message",
+			logLevel: fw.LogTrace,
+			now:      "2020-01-04T10:20:04-00:00",
+			prefix:   "Prefix",
+			messages: []string{"message 1", "message 2"},
+			callers:  []fw.Caller{{}, {}, {FullFilename: "app/test.go", LineNumber: 10}},
+			expectedOutput: `[Prefix] [Trace] 2020-01-04 10:20:04 line 10 at app/test.go message 1
+[Prefix] [Trace] 2020-01-04 10:20:04 line 10 at app/test.go message 2
+`,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			now, err := time.Parse(time.RFC3339, testCase.now)
+			mdtest.Equal(t, nil, err)
+			timer := mdtest.NewTimerFake(now)
+
+			programRuntime, err := mdtest.NewProgramRuntimeFake(testCase.callers)
+			mdtest.Equal(t, nil, err)
+
+			var buf bytes.Buffer
+			logger := NewLocal(
+				testCase.prefix,
+				testCase.logLevel,
+				&buf,
+				timer,
+				programRuntime,
+			)
+
+			for _, message := range testCase.messages {
+				logger.Trace(message)
+			}
+			gotOutput := buf.String()
+			mdtest.Equal(t, testCase.expectedOutput, gotOutput)
+		})
+	}
+}
