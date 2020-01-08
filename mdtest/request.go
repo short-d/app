027c64dd9dda5f5d4895922e1d5d78367422b73b
Harry Liu <byliuyang11@gmail.com>
2020-01-07 23:46:25 -0800

    Update NewHTTPRequestFake

--- mdtest/request.go
+++ mdtest/request.go
@@ -1,10 +1,7 @@
 package mdtest
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"net/http"
-	"strings"
 
 	"github.com/short-d/app/fw"
 	"github.com/short-d/app/modern/mdrequest"
@@ -20,7 +17,7 @@ func (r TransportFake) RoundTrip(req *http.Request) (*http.Response, error) {
 	return r.handle(req)
 }
 
-func NewGraphQLRequestFake(handleFunc TransportHandleFunc) mdrequest.GraphQL {
+func NewGraphQLRequestFake(handleFunc TransportHandleFunc) fw.GraphQlRequest {
 	client := http.Client{
 		Transport: TransportFake{
 			handle: handleFunc,
@@ -28,32 +25,10 @@ func NewGraphQLRequestFake(handleFunc TransportHandleFunc) mdrequest.GraphQL {
 	return mdrequest.NewGraphQL(mdrequest.NewHTTP(client))
 }
 
-func JSONResponse(jsonObj map[string]interface{}) (*http.Response, error) {
-	jsonStr, err := json.Marshal(jsonObj)
-	if err != nil {
-		return nil, err
-	}
-
-	body := ioutil.NopCloser(strings.NewReader(string(jsonStr)))
-	return &http.Response{
-		StatusCode:    http.StatusOK,
-		Body:          body,
-		ContentLength: int64(len(jsonStr)),
-	}, nil
-}
-
-var _ fw.HTTPRequest = (*HTTPRequestFake)(nil)
-
-type HTTPRequestFake struct {
-	err error
-}
-
-func (h HTTPRequestFake) JSON(method string, url string, headers map[string]string, body string, v interface{}) error {
-	return h.err
-}
-
-func NewHTTPRequestFake(err error) HTTPRequestFake {
-	return HTTPRequestFake{
-		err: err,
-	}
+func NewHTTPRequestFake(handleFunc TransportHandleFunc) fw.HTTPRequest {
+	client := http.Client{
+		Transport: TransportFake{
+			handle: handleFunc,
+		}}
+	return mdrequest.NewHTTP(client)
 }
