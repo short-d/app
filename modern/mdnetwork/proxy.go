5cb366da2f7b6a5bf31b1e4fb8a2739f099cfe80
Harry Liu <byliuyang11@gmail.com>
2020-04-19 01:07:26 -0700

    Support extracting connection metadata through proxy protocol

---
+++ modern/mdnetwork/proxy.go
@@ -0,0 +1,23 @@
+package mdnetwork
+
+import (
+	"net/http"
+
+	"github.com/short-d/app/fw"
+)
+
+var _ fw.Network = (*Proxy)(nil)
+
+type Proxy struct {
+}
+
+func (p Proxy) FromHTTP(request http.Request) fw.Connection {
+	clientIP := request.Header.Get("X-Forwarded-For")
+	host := request.Header.Get("X-Forwarded-Host")
+	proto := request.Header.Get("X-Forwarded-Proto")
+	return fw.Connection{
+		ClientIP:      clientIP,
+		RequestedHost: host,
+		Protocol:      proto,
+	}
+}
